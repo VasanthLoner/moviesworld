50245f55b5c717d33dd7961412888e080f39a5e9
Thamarairajan <thamarairajan7@gmail.com>
2022-12-20 18:35:58 +0530

    added movies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Movies from './pages/movies/Movies';
-<<<<<<< HEAD
 import Navbar from './components/navbar/Navbar';
-=======
-import TvShows from './pages/tv-shows/TvShows';
-
->>>>>>> fb74573265c49354744b6e27baf45c05b3899221
 function App() {
   return (
     <>
@@ -17,7 +12,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/movies' element={<Movies />} />
-          <Route path='/tv-shows' element={<TvShows />} />
         </Routes>
       </BrowserRouter>
     </>
